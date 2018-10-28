0f39f9cacba03a20ed1574b83992c6dd378b6c1c
seongjin0526 <sungjin0526@gmail.com>
2018-10-28 15:20:53 +0000

    fix:add login page csrf_token

---
+++ (old)make.sql
@@ -0,0 +1,21 @@
+create database blog;
+use database;
+create table user (
+seq int primary key auto_increment,
+id varchar(255) not null,
+password varchar(255) not null,
+regist_date datetime default now(),
+modify_date datetime default null,
+delete_yn varchar(10) default 'Y'
+);
+
+create table user_login_attempt(
+seq int primary key auto_increment,
+user_id int not null,
+attempt int default 1,
+regist_date datetime default now(),
+modify_date datetime default null
+);
+
+insert into user(id, password) values ('test', 'test');
+
